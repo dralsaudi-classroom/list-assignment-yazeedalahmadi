1d795c30cfa84562a91f61f2a89bc580b78e3611
yazeedalahmadi <alahmadiyazeed77@gmail.com>
2024-09-30 22:37:44 +0300

    Update LinkedList.java

--- src/main/java/com/example/project/LinkedList.java
+++ src/main/java/com/example/project/LinkedList.java
@@ -55,11 +55,17 @@ public class LinkedList<T> implements List<T>{
         }
     }
     public T mostFrequentElement() {
+        //         Write the method mostFrequentElement, member of the class LinkedList, that returns
+        // the most frequent element in the list. The most frequent element is the element
+        // appearing the highest number of times. If one or more element appear the same
+        // number of times, the one encountered earlier is returned.
+        // Example 1.1. Given the list l : A, B, C, B, C, D, E, mostFrequentElement() returns
+        // B.
         T mostFrequent=null;
         int count;int max=0;
         Node<T> a =head;
         while(a!=null){
-            Node<T> b =head;
+            Node<T> b =a;
             count=0;
             while(b!=null){
                 if(b.data.equals(a.data)){
@@ -69,7 +75,7 @@ public class LinkedList<T> implements List<T>{
             }
             if ( count > max ) {
                 max=count;
-                mostFrequent = a.data;
+                mostFrequent = max;
                 }
             a=a.next;
             
